5704ebbbd420855246f6e01215f4dd0751bed6e9
souljavan <jcavanilles@gmail.com>
2021-10-21 20:30:43 +0200

    Iteration#3

---
+++ iteracion3.js
@@ -0,0 +1,21 @@
+//1.1
+let sum = 10*5;
+alert(sum);
+
+//1.2
+let division=10/2;
+alert(division);
+
+//1.3
+let resto=15%9;
+alert(resto);
+
+//1.4
+let y = 10 
+let z = 5
+let x= y+z
+
+//1.5
+let y = 10 
+let z = 5
+let x= y*z
