6795dbb6939b905c1d6f977e1f4e526d3c140cdf
souljavan <jcavanilles@gmail.com>
2021-10-21 21:12:21 +0200

    Iteration#6

---
+++ iteracion6.js
@@ -0,0 +1,22 @@
+//1.1
+for (let i=1;i<10;i++){
+console.log(i)
+}
+
+//1.2
+
+for (let i=1;i<10;i++){
+    if (i%2==0){
+        console.log(i)
+    }
+}
+
+//1.3
+
+for (let i=1;i<11;i++){
+  if (i<10){
+    console.log('Intentando Dormir')
+  }else{
+    console.log('Dormido!')
+  }
+}
